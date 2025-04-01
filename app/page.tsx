e57c8adce38eccc835d0a7b508d85d10d3d9a52f
mac <mac@macs-MacBook-Pro.local>
2025-04-01 22:59:04 +0100

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { toast, Toaster } from 'react-hot-toast';
 export default function Home() {
   const [city, setCity] = useState('');
   const [loading, setLoading] = useState(false);
+  const [loading2, setLoading2] = useState(false);
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
@@ -33,6 +34,31 @@ export default function Home() {
     }
   };
 
+  const handleSendEmails = async (e: any) => {
+    e.preventDefault();
+    setLoading2(true);
+
+    try {
+      const response = await fetch(
+        'https://yeahblend.app.n8n.cloud/webhook/56a56aa7-12e6-4223-948b-ca536b81ccf1',
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' }
+        }
+      );
+
+      if (response.ok) {
+        toast.success('Workflow started!');
+      } else {
+        toast.error('Failed to submit!');
+      }
+    } catch (error) {
+      toast.error('An error occurred. Please try again.');
+    } finally {
+      setLoading2(false);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 p-6">
       <Toaster position='top-right' />
@@ -49,12 +75,23 @@ export default function Home() {
           />
           <button
             type="submit"
-            className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-3 rounded-lg transition duration-300 flex items-center justify-center"
+            className="bg-blue-600 hover:bg-blue-700 cursor-pointer text-white font-semibold px-6 py-3 rounded-lg transition duration-300 flex items-center justify-center"
             disabled={loading}>
             {loading ? (
               <svg className="animate-spin h-5 w-5 mr-3 border-2 border-white border-t-transparent rounded-full" viewBox="0 0 24 24"></svg>
             ) : (
               'Submit'
+            )}
+          </button>
+          <button
+            type="button"
+            onClick={handleSendEmails}
+            className="bg-yellow-600 hover:bg-yellow-700 cursor-pointer text-white font-semibold px-6 py-3 rounded-lg transition duration-300 flex items-center justify-center"
+            disabled={loading2}>
+            {loading2 ? (
+              <svg className="animate-spin h-5 w-5 mr-3 border-2 border-white border-t-transparent rounded-full" viewBox="0 0 24 24"></svg>
+            ) : (
+              'Send Emails'
             )}
           </button>
         </form>
